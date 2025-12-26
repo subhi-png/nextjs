05670a63e85172c774fa3483b3bf188fd6c4e50b
subhi-png <subhi@apollo.lv>
2025-12-26 22:40:46 +0200

    Disable Turbopack in Next.js config
    Disable Turbopack and revert to stable Webpack in Next.js configuration.

--- app/next.config.js
+++ app/next.config.js
@@ -1,1 +1,10 @@
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Отключаем Turbopack, возвращаемся к стабильному Webpack
+  experimental: {
+    turbopack: false,
+  },
+};
+
+module.exports = nextConfig;
